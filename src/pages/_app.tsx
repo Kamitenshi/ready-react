30d16aa762f5c970e7868807886d035e99c4ac76
Mikaël Simarik <mikael.simarik@epita.fr>
2020-04-07 00:55:22 +0200

    SWR: add support

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,10 @@
 import NextApp from 'next/app'
 import React from 'react'
 import { Provider } from 'react-redux'
+import { SWRConfig } from 'swr'
+import fetch from 'unfetch'
 import store from '../utils/store'
+
 interface Props {
     Component: any,
     router: any,
@@ -13,12 +16,23 @@ class App extends NextApp {
         const { Component, pageProps } = this.props
         const header = <h2>Header</h2>
         const footer = <h2>Footer</h2>
+
+        const backUrl = process.env.back_url
+        const swrConfig = {
+            refreshInterval: 3000,
+            fetcher: (service: string) => fetch(`https://${backUrl}/${service}`).then(res => res.json())
+        }
+
         return (
-            <Provider store={store}>
-                {header}
-                <Component {...pageProps} />
-                {footer}
-            </Provider>
+            <SWRConfig
+                value={swrConfig}
+            >
+                <Provider store={store}>
+                    {header}
+                    <Component {...pageProps} />
+                    {footer}
+                </Provider>
+            </SWRConfig>
         )
     }
 }
